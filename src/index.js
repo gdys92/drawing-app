07077bd4a3706308aa7a2683b395055c6b8c2125
gdys92 <gdys92@gmail.com>
2021-04-08 13:21:11 -0300

    canvasContext

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CanvasProvider } from "./canvasContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CanvasProvider>
+      <App />
+    </CanvasProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
