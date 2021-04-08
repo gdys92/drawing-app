5e916de38f6de43cc5261a744083e915706854f0
gdys92 <gdys92@gmail.com>
2021-04-08 12:57:28 -0300

    app.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import React from 'react'
+import { Canvas } from './Canvas'
+import { ClearCanvasButton } from './ClearCanvasButton';
+
+
+function App() {
+
+  return (
+  <>
+    <Canvas/>
+    <ClearCanvasButton/>
+  </>
+  );
+}
+
+export default App;
